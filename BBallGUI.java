9dff7ea111727dee7eb38ad03ec54e0c28e1f6bd
AdamAguir <71364286+AdamAguir@users.noreply.github.com>
2021-04-13 00:13:10 -0600

    More stuff

--- BBallGUI.java
+++ BBallGUI.java
@@ -2,11 +2,20 @@ import java.awt.Color;
 
 import javax.swing.JFrame;
 
+import jdk.internal.platform.Container;
+
 public class BBallGUI extends JFrame{
    
     public BBallGUI(){
         setSize(300,300);
         setBackground(Color.cyan);
+        setResizable(false);
+
+        BorderLayout layout = new BorderLayout();
+        Container pane = getContentPane();
+        pane.setLayout();
+
+        pane.add(new BBallPanel());
     }
 
     public static void main(String[] args) {
--- BBallPanel.java
+++ BBallPanel.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class BBallPanel extends JPanel {
@@ -6,9 +8,26 @@ public class BBallPanel extends JPanel {
 private ArrayList<Player> players = new ArrayList<Player>();
 
     public BBallPanel(){
+        JLabel fNameLBL = new JLabel("First Name");
+        JLabel lNameLBL = new JLabel("Last Name");
+        JLabel shootPctLBL = new JLabel("Shooting %");
+        JLabel reboundsLBL = new JLabel("Rebounds");
+        JLabel assistsLBL = new JLabel("Assists");
+        JLabel turnoversLBL = new JLabel("Turnovers");
+        JLabel currentLBL = new JLabel("0 of 0");
+
+
+        add(fNameLBL);
+        add(lNameLBL);
+        add(shootPctLBL);
+        add(reboundsLBL);
+        add(assistsLBL);
+        add(turnoversLBL);
+        add(currentLBL);
 
     }
     private void newRecord(){
+
     }
 
     private void saveRecord(){
